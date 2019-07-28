7893c29b62bef3d5604a2da18b31b1a9d45169cc
wenai <wenai@mafengwo.com>
2019-07-28 23:32:25 +0800

    add webpack.prod.config.js

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -2,19 +2,23 @@ const path = require('path');
 const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const ROOT_PATH = path.resolve(__dirname, '../');
 const SRC_PATH = path.resolve(ROOT_PATH, 'src');
-const BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+const BUILD_PATH = path.resolve(ROOT_PATH, 'public');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   context: ROOT_PATH,
+  stats: {
+    children: false,
+  },
   entry: {
     app: path.resolve(SRC_PATH, 'app.js'),
   },
   output: {
-    filename: '[name].js',
+    filename: 'js/[name].[hash].js',
     path: BUILD_PATH,
   },
   resolve: {
@@ -38,10 +42,14 @@ module.exports = {
     }]
   },
   plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['js/*.js'],
+    }),
     new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       title: '',
-      filename: 'index.html',
+      filename: 'html/index.html',
       template: path.resolve(SRC_PATH, 'index.html'),
     }),
   ]
